0e8057f7e8aca3710a87421b64eda425b55ec7e8
FernandoUsurin <83975104+FernandoUs@users.noreply.github.com>
2024-07-06 12:51:24 -0500

    Add files via upload

--- crear_carpeta.py
+++ crear_carpeta.py
@@ -7,12 +7,15 @@ def lambda_handler(event, context):
     nombre_bucket = info_json['bucket']
     nombre_carpeta = info_json['carpeta']
     
-    # Proceso
+# Proceso
     s3 = boto3.client('s3')
-    s3.put_object(Bucket=nombre_bucket, Key=nombre_carpeta+'/')
-    
+    s3.put_object(Bucket=nombre_bucket, Key=f'{nombre_carpeta}/')
+        
     return {
         'statusCode': 200,
-        'bucket': nombre_bucket,
-        'carpeta': nombre_carpeta
+        'body': json.dumps({
+            'message': 'Carpeta creada exitosamente',
+            'bucket': nombre_bucket,
+            'carpeta': nombre_carpeta
+        })
     }
